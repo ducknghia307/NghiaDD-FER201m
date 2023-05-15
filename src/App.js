dde7f903f2e406454968ccdeecd6005d7b7dbce6
Duc Nghia <ducnghia30703@gmail.com>
2023-05-15 10:27:07 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Navigation from './components/Navigation';
 import Player from './components/Players'
 import Footer from './components/Footer';
 import Players from './components/Players';
+import { Main } from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
-      <Players/>
+      <Main></Main>
       <Footer/>
     </div>
   );
