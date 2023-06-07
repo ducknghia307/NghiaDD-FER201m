970ab91699867da9cd9a05dcbe6739c9049cb115
Duc Nghia <ducnghia30703@gmail.com>
2023-06-07 20:56:28 +0700

    ex11

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from './ThemeContext'
 export default function Navigation(){
-  
+    const { theme, toggle, dark } = useContext(ThemeContext)
         return(
-            
-                <nav >
-                    <ul class="navigator" >
-                        <li  class="navigator_item" ><a className='active' href='#home'>Home</a></li>
-                        <li class="navigator_item"><a href='#news'>News</a></li>
-                        <li class="navigator_item"><a href='#about'>About</a></li>
-                        <li class="navigator_item"><a href='#contact'>Contact</a></li>
-                    </ul>
+            <div>
+                <nav style={{ backgroundColor: theme.backgroundColor, color: theme.color,display:"flex",justifyContent: "space-between", }} >
+                    <ul className='navigator'>
+                        <li   class="navigator_item" ><a    style={{
+                                
+                                color: theme.color,
+                                outline: "none",
+                            }} className='active' href='#home'>Home</a></li>
+                        <li class="navigator_item"><a    style={{
+                                
+                                color: theme.color,
+                                outline: "none",
+                            }} href='#news'>News</a></li>
+                        <li class="navigator_item"><a    style={{
+                                
+                                color: theme.color,
+                                outline: "none",
+                            }}  href='#about'>About</a></li>
+                        <li class="navigator_item"><a    style={{
+                                
+                                color: theme.color,
+                                outline: "none",
+                            }} href='#contact'>Contact</a></li>
+                        </ul>
+                        <div style={{ position: "relative", padding: 10 }}>
+                    <a
+                        className="switch-mode"
+                        href="#"
+                        onClick={toggle}
+                        style={{
+                            backgroundColor: theme.backgroundColor,
+                            color: theme.color,
+                            paddingRight: 40,
+                            textDecoration: "none",
+                        }}
+                        data-testid="toggle-theme-btn"
+                    >
+                        <span>Switch Nav to {!dark ? "Dark" : "Light"} mode</span>
+                    </a>
+         </div>
+
+                    
                 </nav>
-                
+                </div>    
     
             
         )
     }
+  
